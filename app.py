287e8b29a54b5d4a8f9f8d249d9522692407861a
hellorobo <trebor.oknewokaj@gmail.com>
2018-05-06 09:16:40 +0200

    application prepared for deployment

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ api.add_resource(LineNameId, '/lineid/<string:linename>')
 api.add_resource(Departure, '/departure/<string:lineName>')
 
 if __name__ == '__main__':
-    app.run(port=5002, debug=True)
+    app.run(port=5002)
